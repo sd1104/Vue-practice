7372b0b0b8ae70aab18f60092103d13b3f369971
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-31 19:38:27 +0900

    Update main.js

--- to-do-app/js/main.js
+++ to-do-app/js/main.js
@@ -1,23 +1,6 @@
-new Vue ({
+new Vue({
   el: '#app',
   data: {
-    newItem: '',
-    todos: []
-  },
-  methods: {
-    addItem: function() {
-      if (this.newItem === '') {
-        return;
-      }
-      let todo = {
-        item: this.newItem,
-        isDone: false
-      }
-      this.todos.push(todo);
-      this.newItem = '';
-    },
-    deleteItem: function(index) {
-      this.todos.splice(index, 1);
-    }
+    message: 'hello'
   }
 })
