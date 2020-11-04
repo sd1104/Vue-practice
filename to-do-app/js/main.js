543ebf0df75a223bad42b37e0d08173daa8614b2
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-04 11:59:35 +0900

    Update main.js

--- to-do-app/js/main.js
+++ to-do-app/js/main.js
@@ -4,5 +4,13 @@ new Vue({
     bpi: null,
     error: false,
     loading: true
+  },
+  mounted: function() {
+    axios
+    .get ('https://api.coindesk.com/v1/bpi/currentprice.json')
+
+    .then(function(response) {
+      this.bpi = response.data.bpi
+    })
   }
 })
