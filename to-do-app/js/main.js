95eba5390e8a824e2a4bafc73678319cfb50e6ef
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-09 15:13:44 +0900

    Update main.js

--- to-do-app/js/main.js
+++ to-do-app/js/main.js
@@ -1,30 +1,6 @@
-const todo = new Vue({
+new Vue({
   el: '#app',
   data: {
-    bpi: null,
-    error: false,
-    loading: true
-  },
-  mounted() {
-    axios
-    .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-
-    .then(function(response){
-      this.bpi = response.data.bpi;
-    }.bind(this))
-
-    .catch(function(error){
-      console.log(error);
-      this.error = true;
-    }.bind(this))
-
-    .finally(function(){
-      this.loading = false;
-    }.bind(this))
-  },
-  filters: {
-    fixed_two(value){
-      return value.toFixed(2);
-    }
+    message: 'hello Vue.js'
   }
 })
