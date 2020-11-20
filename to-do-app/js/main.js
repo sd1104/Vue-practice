087b5bf28d73c073fa2cefeb41608ee5d5114768
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-20 11:04:04 +0900

    Update main.js

--- to-do-app/js/main.js
+++ to-do-app/js/main.js
@@ -12,5 +12,10 @@ new Vue({
     .then(function(response) {
       this.bpi = response.data.bpi
     }.bind(this))
+
+    .catch(function(error) {
+      console.log(error);
+      this.error = true;
+    }.bind(this))
   }
 })
