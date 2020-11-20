b34467af1059473dc099b0a6558fceba54032069
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-20 10:58:32 +0900

    reset

--- to-do-app/js/main.js
+++ to-do-app/js/main.js
@@ -1,21 +1,7 @@
 new Vue({
   el: '#app',
   data: {
-    newData: '',
-    todos: []
-  },
-  methods: {
-    addItem: function() {
-      if(this.newItem === '') return;
-      todo = {
-        item: this.newItem,
-        isDone: false
-      }
-      this.todos.push(todo);
-      this.newItem = '';
-    },
-    deleteItem: function(index) {
-      this.todos.splice(index, 1);
-    }
+
+  }
   }
 })
