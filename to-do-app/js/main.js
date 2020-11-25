eccf41f9c26637957704d9843a6d4ca34dff47c2
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-25 12:26:08 +0900

    Update main.js

--- to-do-app/js/main.js
+++ to-do-app/js/main.js
@@ -1,21 +1,8 @@
 new Vue({
   el: '#app',
   data: {
-    newItem: '',
-    todos: []
-  },
-  methods: {
-    addItem: function() {
-      if(this.newItem === '') return;
-      todo = {
-        item: this.newItem,
-        isDone: false
-      }
-      this.todos.push(todo);
-      this.newItem = '';
-    },
-    deleteItem: function(index) {
-      this.todos.splice(index, 1)
-    }
+    bpi = null,
+    error = false,
+    loading = true
   }
 })
