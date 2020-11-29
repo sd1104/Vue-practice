e1e953f1390b8002cc4923f78ba678025b7c0222
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-29 22:40:38 +0900

    Update main.js

--- to-do-app/js/main.js
+++ to-do-app/js/main.js
@@ -12,5 +12,19 @@ new Vue({
     .then(function(response) {
       this.bpi = response.data.bpi
     }.bind(this))
+
+    .catch(function(error) {
+      console.log(error);
+      this.error=true;
+    }.bind(this))
+
+    .finally(function() {
+      this.loading = false
+    }.bind(this))
+  },
+  filters: {
+    fixed: function(value) {
+      return value.toFixed(2);
+    }
   }
 })
